d3000a85c34f63bf65db1fa801bcb1f685b79a0d
Orta Therox <git@orta.io>
2025-02-08 20:39:45 +0000

    Logs

--- _api/ssr_.tsx
+++ _api/ssr_.tsx
@@ -1,17 +1,15 @@
-import { renderToPipeableStream, renderToString } from "react-dom/server";
+import { renderToPipeableStream } from "react-dom/server";
 import { OtherFile } from "../src/OtherFile";
 import React from "react"
 
 export default async function handler(request, response) {
-  console.time("handler");
   const { pipe } = renderToPipeableStream(<OtherFile />, {
     onShellReady() {
       response.setHeader('content-type', 'text/html');
       pipe(response);
+    },
+    onError(error) {
+      console.error(error);
     }
   });
-  console.timeEnd("handler");
-  // return new Response("asda", {
-  //   status: 200,
-  // });
 }
