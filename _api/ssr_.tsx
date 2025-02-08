f01e61bdf23c7dda1cdd8d74038cc038684a7982
Orta Therox <git@orta.io>
2025-02-08 20:20:36 +0000

    TSX isr

--- _api/ssr_.tsx
+++ _api/ssr_.tsx
@@ -1,9 +1,10 @@
 import { renderToString } from "react-dom/server";
 import { OtherFile } from "../src/OtherFile";
-// export const edge = true;
+import React from "react"
 
 export default async function handler() {
-  const appHtml = renderToString(OtherFile());
+  console.log("Start")
+  const appHtml = renderToString(<OtherFile />);
   return new Response(appHtml, {
     status: 200,
   });
