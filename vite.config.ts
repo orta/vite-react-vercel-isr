e0c99b65f29fad515889b36c9c66d22a945e1a2c
Orta Therox <git@orta.io>
2025-02-08 20:00:33 +0000

    Fix ISR

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import vercel from 'vite-plugin-vercel';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import vercel from "vite-plugin-vercel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),vercel()],
-  
+  plugins: [react(), vercel()],
+
   vercel: {
     isr: {
-      '/pages/a': { expiration: 15,  route: '^/a/.*$' },
-    }
-  }
-})
+      "/pages/a": { expiration: 15, symlink: "api/ssr_", route: "^/a/.*$" },
+      "/pages/b/c": { expiration: 15, symlink: "api/ssr_", route: "^/b/c/.*$" },
+      "/pages/d": { expiration: 15, symlink: "api/ssr_", route: "^/d$" },
+      "/pages/e": { expiration: 25 },
+    },
+  },
+});
