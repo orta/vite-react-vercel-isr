7182682a52cc0db737284d0f445ab470266c3cdb
Orta Therox <git@orta.io>
2025-02-08 20:06:23 +0000

    Try edge render a react component

--- _api/ssr_.ts
+++ _api/ssr_.ts
@@ -1,8 +1,10 @@
+import { renderToString } from "react-dom/server";
+import { OtherFile } from "../src/OtherFile";
 export const edge = true;
 
 export default async function handler() {
-    return new Response('Edge Function: OK', {
-      status: 200,
-    });
-  }
-  
+  const appHtml = renderToString(OtherFile());
+  return new Response(appHtml, {
+    status: 200,
+  });
+}
